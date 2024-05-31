4dd19303621967e5621fcdb7e9b5c27770fad60f
razeeth11 <razeeth011@gmail.com>
2024-05-31 12:15:55 +0530

    'secret'

--- index.js
+++ index.js
@@ -6,28 +6,11 @@ import router from "./newFile.js";
 dotenv.config();
 export const app = express();
 const PORT = 4000;
-export const SECRET_KEY = process.env.SECRET_KEY;
+export const secretKey = process.env.SECRET_KEY;
 const MONGO_URL = process.env.MONGO_URL;
 
-
-export let client;
-
-async function initializeMongoClient() {
-  try {
-    client = new MongoClient(MONGO_URL);
-    await client.connect();
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.error("Failed to connect to MongoDB", error);
-    process.exit(1);  // Exit process if the connection fails
-  }
-}
-
-initializeMongoClient();
-
-
-// export const client = new MongoClient(MONGO_URL);
-// client.connect();
+export const client = new MongoClient(MONGO_URL);
+client.connect();
 
 app.use(express.json());
 app.use(cors());
